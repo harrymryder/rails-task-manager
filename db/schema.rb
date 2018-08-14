defd18f498c446ee16cef8de442f594479e0b1dd
Harry Ryder <harrymryder@gmail.com>
2018-08-14 16:42:53 +0100

    Task manager complete

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_14_104145) do
+ActiveRecord::Schema.define(version: 2018_08_14_105730) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "completed", default: false
   end
 
 end
